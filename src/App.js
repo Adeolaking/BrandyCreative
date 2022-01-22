aa356b5440f9b761ee8525b66904440843af354b
Abdurahmon Adeolaking <seyeologo14@gmail.com>
2022-01-22 08:55:22 +0100

    first deployment

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="main-nav-cont">
+        <div className="logo"></div>
+        <ul>
+          <li>Home</li>
+          <li>Services</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <header className="App-header"> 
+        <h3>welcome to</h3>
+        <h1> Brandy <span> Creative</span></h1>
+        <h2>we are one of the most effective Creative Design Companies</h2>
+        <div className='btn'>
+        <button className="main-btn">GET STARTED</button>
+        <button className="main-btn">SERVICES</button>
+        </div>
+      </header>
+      <main>
+          <section>
+            
+          </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
